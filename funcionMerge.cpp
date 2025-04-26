6f246cdfcf1c8ffb81a901ebfb6a7fbe1b8738e1
Mathortas <elderiva83@gmail.com>
2025-04-25 21:55:43 -0500

    merge del profe

--- funcionMerge.cpp
+++ funcionMerge.cpp
@@ -1,65 +1,40 @@
 #include <iostream>
+using namespace std;
 
-void merge(int* arr, int n) {
-
-    bool empiezaConPares = (arr[0] % 2 == 0);
-    //Puntero de referencia para no pasarse de par a impar o de impar a par o fuera de indice :V
-    int* ptrSegundaMitad = arr;
-    
-    while (ptrSegundaMitad < arr + n && ((*ptrSegundaMitad % 2 == 0) == empiezaConPares)) {
-        ptrSegundaMitad++;
-    }
-    
-    // Crear array auxiliar
-    int aux[n];
-    int* ptrAux = aux;
-    
-    // Punteros para pares e impares
-    int* ptr1 = arr;
-    int* ptr2 = ptrSegundaMitad;
-    
-    // Comparar cual es menor, el menor lo manda al aux y de ahi avanzar el ptr del menor hasta el final
-    while (ptr1 < ptrSegundaMitad && ptr2 < arr + n) {
-        if (*ptr1 <= *ptr2) {
-            *ptrAux++ = *ptr1++;
+void Merge(int *p, int *q) {
+    int *fin = p + (q - p) * 2;
+    while (p < q && q < fin) {
+        if (*p < *q) {
+            p++;
         } else {
-            *ptrAux++ = *ptr2++;
+            // Rotar elementos desde p hasta q
+            for (int *r = p; r < q; r++) {
+                swap(*r, *q);
+            }
+            q++;
+            p++;
         }
     }
-    
-    // Copiar los elementos restantes de pares/impares
-    while (ptr1 < ptrSegundaMitad) {
-        *ptrAux++ = *ptr1++;
+}
+void imprimirArray(int *arr, int size) {
+    for (int *ptr = arr; ptr < arr + size; ptr++) {
+        cout << *ptr << " ";
     }
-    
-    // Copiar los elementos restantes de impares/pares
-    while (ptr2 < arr + n) {
-        *ptrAux++ = *ptr2++;
-    }
-    
-    // Copiar del auxiliar al original
-    for (int i = 0; i < n; i++) {
-        arr[i] = aux[i];
-    }
+    cout << endl;
 }
 
 int main() {
-    int arr[] = {2, 4 , 6 , 1 , 3 , 5 ,7 };
+    int arr[] = {1, 3, 5, 7, 2, 4, 6, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
-    
-    std::cout << "Array original: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
+    int *mitad = arr;
+    while (mitad < arr + n && *mitad % 2 != 0) {
+        mitad++;
     }
-    std::cout << std::endl;
-    
-    merge(arr, n);
-    
-    std::cout << "Array ordenado: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
+    cout << "Array original:" << endl;
+    imprimirArray(arr, n);
+    Merge(arr, mitad);
+    cout << "Array ordenado:" << endl;
+    imprimirArray(arr, n);
     
     return 0;
 }
