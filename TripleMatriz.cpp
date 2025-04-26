8cb11183af0331a8a3c007f43a445173a3b6f3a1
Mathortas <elderiva83@gmail.com>
2025-04-26 08:44:24 -0500

    ERA MULTIPLICAR :V,

--- TripleMatriz.cpp
+++ TripleMatriz.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 const int DIM = 3;
-void sumarMatrices(int (*A)[DIM], int (*B)[DIM], int (*resultado)[DIM]) {
-    for(int (*a)[DIM] = A, (*b)[DIM] = B, (*r)[DIM] = resultado; a < A + DIM; a++, b++, r++) {
-        for(int *ea = *a, *eb = *b, *er = *r; ea < *a + DIM; ea++, eb++, er++) {
-            *er = *ea + *eb;
+
+void multiplicarMatrices(int (*A)[DIM], int (*B)[DIM], int (*resultado)[DIM]) {
+    for(int (*a)[DIM] = A, (*r)[DIM] = resultado; a < A + DIM; a++, r++) {
+        for(int *er = *r; er < *r + DIM; er++) {
+            *er = 0; // Inicializar el elemento del resultado
+            for(int k = 0; k < DIM; k++) {
+                *er += (*(*a + k)) * (*(*(B + k) + (er - *r)));
+            }
         }
     }
 }
@@ -25,11 +29,12 @@ void imprimirMatriz3D(int (*matriz3D)[DIM][DIM]) {
 
 int main() {
     int matrices[3][DIM][DIM] = {
-        {{11, 12, 13}, {14, 15, 16}, {17, 18, 19}},  // Matriz A
+        {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},  // Matriz A
         {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}},  // Matriz B
-        {{}}                               // Espacio para suma
+        {{}}                                // Espacio para resultado
     };
-    sumarMatrices(matrices[0], matrices[1], matrices[2]);
+    
+    multiplicarMatrices(matrices[0], matrices[1], matrices[2]);
     imprimirMatriz3D(matrices);
 
     return 0;
