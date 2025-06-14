09f4ef6db7d5c6efe76bd3b6d75b47cd8e991813
Mathortas <elderiva83@gmail.com>
2025-06-14 08:44:04 -0500

    Actualizar figuras.cpp

--- figuras.cpp
+++ figuras.cpp
@@ -1,48 +1,59 @@
 #include <iostream>
 using namespace std;
 
+
 float rectangulo(float base, float altura) {
-    cout << "El area del rectangulo es: " << base * altura << endl;
     return base * altura;
 }
 
 float circulo(float radio, float) {
-    float pi = 3.1416;
-    cout << "El area del circulo es: " << pi * (radio * radio) << endl;
-    return pi * (radio * radio);
+    const float pi = 3.1416;
+    return pi * radio * radio;
 }
 
 float triangulo(float base, float altura) {
-    cout << "El area del triangulo es: " << (base * altura) / 2 << endl;
     return (base * altura) / 2;
 }
 
 float cuadrado(float lado, float) {
-    cout << "El area del cuadrado es: " << lado * lado << endl;
     return lado * lado;
 }
 
-typedef float (*PF)(float, float); // puntero a función
+
+typedef float (*PF)(float, float);
+
+
+void ejecuta(PF* funciones, float (*parametros)[2], char (*nombres)[11]) {
+    for (int i = 0; i < 4; i++) {
+        float valor1 = *(*(parametros + i) + 0);
+        float valor2 = *(*(parametros + i) + 1);
+        float area = (*(funciones + i))(valor1, valor2);
+
+        
+        char* ptrNombre = *(nombres + i);
+        cout << "Figura: ";
+        while (*ptrNombre != '\0') {
+            cout << *ptrNombre;
+            ptrNombre++;
+        }
+        cout << " - Área: " << area << " metros" << endl;
+    }
+}
+
 
 int main() {
     PF funciones[4] = {rectangulo, circulo, triangulo, cuadrado};
 
-    float figuras[4][2] = {
-        {2, 0},   // para cuadrado (indice 3)
-        {3, 5},   // para triangulo (indice 2)
-        {10, 0},  // para circulo (indice 1)
-        {5, 3}    // para rectangulo (indice 0)
+    float parametros[4][2] = {
+        {5.0, 3.0},   // rectángulo
+        {4.0, 0.0},   // círculo
+        {6.0, 2.0},   // triángulo
+        {3.5, 0.0}    // cuadrado
     };
 
-    int ordenFiguras[4] = {3, 2, 1, 0}; // índice de funciones
+    char nombres[4][11] = {"rectangulo", "circulo", "triangulo", "cuadrado"};
 
-    for (int i = 0; i < 4; i++) {
-        int idx = ordenFiguras[i];
-        float base = figuras[i][0];
-        float altura = figuras[i][1];
-
-        funciones[idx](base, altura);
-    }
+    ejecuta(funciones, parametros, nombres);
 
     return 0;
 }
